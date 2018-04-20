061bc0c9c7b3e8346ddc419c10bef0eaa378bcb9
Julian Johannesen <julian.johannesen@gmail.com>
2018-04-20 19:38:12 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,8 +6,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(i)
-  gets.strip.to_i - 1
+def input_to_index(input)
+  index = input.to_i - 1
 end
 
 def move(board, index, marker="X")
